23eda43eaeb1fd41512ab5383410e12a7bdd67b0
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2025-06-10 23:03:29 +0700

    add cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const Router = require('./router');
 const { PORT } = require('./config/index')
 
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
-
+app.use(cors());
 
 app.use(Router)
 
